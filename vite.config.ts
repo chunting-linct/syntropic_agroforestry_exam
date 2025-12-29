799fd911d90887233a1c7231e493960531315395
chunting-linct <chunting.linct@gmail.com>
2025-12-29 15:21:04 +1100

    Update : vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/syntropic_agroforestry_exam/',
       server: {
         port: 3000,
         host: '0.0.0.0',
